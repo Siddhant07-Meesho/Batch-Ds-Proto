51d0e65215413b3c0070fed5185b8cf7f8c47e21
Siddhant07-Meesho <siddhant.shekhar@meesho.com>
2025-05-15 12:54:36 +0530

    Update sscatMappingNewFormat.py

--- searchPhrase/sscatMappingNewFormat.py
+++ searchPhrase/sscatMappingNewFormat.py
@@ -1,7 +1,32 @@
-import random
-
 import redis
-from debug.searchPhrase import searchPhrase_pb2
+import json
+
+# Initialize Redis client
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+# List of eligible items
+
+oldFormatData = [
+    {
+        "id": 1007,
+        "name": "Blouses",
+        "clp": 88636
+    },
+    {
+        "id": 1093,
+        "name": "Jewellery Set",
+        "clp": 39043
+    },
+    {
+        "id": 1094,
+        "name": "Bangles & Bracelets",
+        "clp": 176832
+    },
+    {
+        "id": 1091,
+        "name": "Earrings & Studs",
+        "clp": 954
+    }]
 
 eligible_sscat = [
     {
@@ -25,71 +50,40 @@ eligible_sscat = [
                 "clp": 39043
             }
         ]
+    },
+    {
+        "group_id":1094,
+        "combined_sscat_name": "Bangles & Bracelets",
+        "sscats_combined":[
+            {
+                "id": 1094,
+                "name": "Bangles & Bracelets",
+                "clp": 176832
+            }
+        ]
+    },
+    {
+        "group_id":1091,
+        "combined_sscat_name": "Earrings & Studs",
+        "sscats_combined":[
+            {
+                "id": 1091,
+                "name": "Earrings & Studs",
+                "clp": 954
+            }
+        ]
     }
 ]
 
-# Sample data generator
-def generate_sample_data(eligible_sscat):
-    descriptions = [
-        "Saree with yellow flower",
-        "Kurti with red border",
-        "Lehenga with silver design",
-        "Salwar with gold thread",
-        "Dupatta with green embroidery",
-        "Top with blue polka dots"
-    ]
-    return [{"id": grp["group_id"], "sp": random.choice(descriptions)} for grp in eligible_sscat]
 
 
 
-# Function : Store using Protocol Buffers (proto format)
-def store_using_protobuf(redis_client, key, data):
-    # Create SearchPhrases message
-    search_phrases = searchPhrase_pb2.SearchPhrases()
+eligible_sscat_json = [json.dumps(item) for item in eligible_sscat]
 
-    # Add groups to search_phrases
-    for item in data:
-        group = search_phrases.phrases.add()
-        group.id = item['id']
-        group.sp = item['sp']
-
-    # Serialize the data to binary format
-    serialized_data = search_phrases.SerializeToString()
-
-    # Store in Redis
-    redis_client.set(key, serialized_data)
-
-
-def read_from_redis(redis_client, key):
-    # Fetch raw binary data
-    serialized_data = redis_client.get(key)
-
-    # Parse it back into Protobuf object
-    search_phrases = searchPhrase_pb2.SearchPhrases()
-    search_phrases.ParseFromString(serialized_data)
-
-    # Print parsed data
-    for group in search_phrases.phrases:
-        print(f"id: {group.id}, sp: {group.sp}")
-
-
-# Example usage
-if __name__ == "__main__":
-    # Connect to Redis
-    r = redis.Redis(host='localhost', port=6379, db=0)
-
-    sample_data = generate_sample_data(eligible_sscat)
-    print(sample_data)
-    purchasedProductId = 8767890
-    purchasedSscatId = 1004
-
-    key = f"SP:v1:{purchasedProductId}"
-
-    # suppose 1004 has sscatMapping of above eligible_sscat
-
-    # Store using Protobuf
-    store_using_protobuf(r, key, sample_data)
-
-    print("Data pushed")
-
-    read_from_redis(r, key)
+# Loop to add data to Redis
+for i in range(500, 550):
+    key = "ES:v3:" + str(i)
+    # Push all items at once
+    redis_client.rpush(key, *eligible_sscat_json)  # Use unpacking to pass all items
+    if i % 500 == 0:
+        print(f"Set eligible sscats for 500 sscats")
